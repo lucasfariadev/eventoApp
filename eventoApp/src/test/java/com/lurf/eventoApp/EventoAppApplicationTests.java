441953fb16b93ea730c16820fc6e014e1c583d9b
lucaslurf <lucas_faria@outlook.com>
2023-02-03 01:16:01 -0300

    adição de estrutura inicial

---
+++ eventoApp/src/test/java/com/lurf/eventoApp/EventoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lurf.eventoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
